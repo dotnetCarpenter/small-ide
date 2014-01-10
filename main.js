d3566da89f764b49f4af417f8e4b777a13f1d9f6
dotnetCarpenter <jon.ronnenberg@gmail.com>
2014-01-10 04:37:52 +0100

    added indentation for non selection

--- main.js
+++ main.js
@@ -6,65 +6,24 @@ var ta = fst($("textarea"));
 var out = fst($("code"));
 var writer = format(out);
 var events = {
-  shift13:run, // shift + enter
-  selection9:indentSelection, // select text + tab
-  shiftselection9:function(selection, bounds){ indentSelection(selection, bounds, true); },
-  shift8:out.clear // shift + backspace KLUDGE: format(out) has to be called before this
+  Öshift13:run, // shift + enter
+  Ö9:indentSelection, // tab
+  Öshift9:function(text, bounds){ indentSelection(text, bounds, true); }, // shift + tab
+  Öselection9:indentSelection, // select text + tab
+  Öshiftselection9:function(text, bounds){ indentSelection(text, bounds, true); }, // shift + select text + tab
+  Öshift8:out.clear // shift + backspace KLUDGE: format(out) has to be called before this
 };
+var config = {
+  indentation: 2
+}
 
 window.addEventListener("error", log);
+
+polyfills();
 
 //a.addEventListener("mouseup", inputWatch(ta)); // mouse selection
 ta.addEventListener("keydown", inputWatch(ta)); // detect tab
 //ta.addEventListener("keypress", inputWatch(ta)); // detect tab
-
-function indentSelection(text, bounds, invert) {
-  // from http://jsbin.com/ijedi
-  var indentation, newText;
-  var start = bounds[0];
-  var end = bounds[1];
-  var removeSpace = /^(\s{1,2})/mg;
-  var addSpace = /^(?=.+)/mg;
-  var affectedLines = 0;
-  // extend the selection start until the previous new line
-  start = text.lastIndexOf("\n", start);
-  // if there isn't a new line before,
-  // then extend the selection until the beginning of the text
-  if(start === -1)
-    start = 0;
-  // if the selection starts with a new line,
-  // remove it from the selection
-  if (text.charAt(start) == "\n")
-    start += 1;
-  // if the selection ends with a new line,
-  // remove it from the selection
-  if (text.charAt(end - 1) == "\n")
-    end -= 1;
-  // extend the selection end until the next new line
-  end = text.indexOf("\n", end);
-  // if there isn't a new line after,
-  // then extend the selection end until the end of the text
-  if (end === -1)
-    end = text.length;
-  // get text to indent
-  indentation = text.substring(start, end);
-  // count number of affected lines
-  affectedLines = invert ? indentation.match(removeSpace).length : indentation.match(addSpace).length;
-  // add two spaces/backspaces before new lines
-  indentation = invert ? indentation.replace(removeSpace, "") : indentation.replace(addSpace, "  ");
-  // rebuild the textarea content
-  newText = text.substring(0, start);
-  newText += indentation;
-  newText += text.substring(end);
-  ta.value = newText;
-  // add/substract end after indentation
-  invert ? (end -= affectedLines*2) : (end += affectedLines*2);
-  // reselect text
-  selectText(start, end);
-}
-function selectText(start, end) {
-  ta.setSelectionRange(start, end);
-}
 
 function KeyEvent(shift, ctrl, alt, selection, charcode, value, extras) {
   this.shift = shift;
@@ -77,7 +36,7 @@ function KeyEvent(shift, ctrl, alt, selection, charcode, value, extras) {
 }
 KeyEvent.prototype.toString = function() {
   var self = this;
-  return Object.keys(this).map(function(key) {
+  return "Ö" + Object.keys(this).map(function(key) {
     var not = ["toString", "value", "extras"];
     var ret = (self[key] === true && key) || self[key];
     return !~not.indexOf(key) && ret || "";
@@ -90,13 +49,13 @@ function inputWatch(field) {
   return function(e) {
     e = e || window.event;
     var charCode = (e.which instanceof Number) ? e.which : e.keyCode;
-    var value = field.value, isSelection = false, extras;
+    var value = field.value, isSelection = false, extras, ke;
     if(field.selectionStart < field.selectionEnd) {
       isSelection = true;
-      extras = [field.selectionStart, field.selectionEnd];
     }
-    var ke = new KeyEvent(e.shiftKey, e.ctrlKey, e.altKey, isSelection, charCode, value, extras);    
-//log(e.keyCode, charCode, ke.toString());
+    extras = [field.selectionStart, field.selectionEnd];
+    ke = new KeyEvent(e.shiftKey, e.ctrlKey, e.altKey, isSelection, charCode, value, extras);    
+// log(ke.toString());
     inputs.push(ke);
     if(events[ke.toString()])
       e.preventDefault();
@@ -110,14 +69,99 @@ function runFunction(inputs) {
   var shortcutFunction;
   inputs.forEach(function(keyevent) {
     shortcutFunction = events[ keyevent.toString() ];
-    shortcutFunction && shortcutFunction(keyevent.value, keyevent.extras);
+    shortcutFunction && window.setTimeout(shortcutFunction, 0, keyevent.value, keyevent.extras);
   });
   emptyArray(inputs);
 }
 
 function emptyArray(list) {
-  list.forEach(function(item) { item = null; });
+  list.forEach(function(item) { return item = null; });
   list.length = 0;
+}
+
+function indentSelection(text, bounds, invert) {
+  /* issues:
+    1. shift+tab at end of line deletes text - should indent if possible or return
+    2. shift at end of all text indent line - should insert spaces
+    3. shift+tab at end of all text throws an error
+  */
+  // from http://jsbin.com/ijedi
+  var indentation, newText;
+  var spaces = " ".repeat(config.indentation || 2);
+  var numberOfIndents = spaces.length;
+  var start = bounds[0];
+  var end = bounds[1];
+  var singleSelection = start === end;
+  var removeSpace = new RegExp("^(\\s{1," + numberOfIndents + "})","mg");
+  var addSpace = /^(?=.+)/mg;///^(?=[^\S\n]|.+)/mg;///^(?=.+)/mg;///^(?=.+|\s)/mg;///^.?/mg;
+  var empty = /^\s*$/g;
+  var affectedLines = 0;
+  // extend the selection start until the previous new line
+  start = text.lastIndexOf("\n", start);
+  // if there isn't a new line before,
+  // then extend the selection until the beginning of the text
+  if(start === -1)
+    start = 0;
+  // if the selection starts with a new line,
+  // remove it from the selection
+  if (text.charAt(start) === "\n")
+    start += 1;
+
+  // if the selection ends with a new line,
+  // remove it from the selection
+  if (text.charAt(end - 1) === "\n")
+    end -= 1;
+  // extend the selection end until the next new line
+  end = text.indexOf("\n", end);
+  // if there isn't a new line after,
+  // then extend the selection end until the end of the text
+  if (end === -1)
+    end = text.length;
+  // get text to indent
+  indentation = text.substring(start, end);
+
+  // if(empty.test(indentation)) {
+  //   affectedLines = 1;
+  //   indentation = invert ? "" : "  " + indentation;
+  // }  
+
+  // test if the indendation selection is empty
+  if(empty.test(indentation)) {
+    affectedLines = 1;
+    if(invert) {
+      // test for line boundary
+
+      indentation = ""; // nothing to indent
+      start = bounds[0] - numberOfIndents; // "loose" n chars
+      end = bounds[0]
+    } else {
+      indentation = spaces;
+      start = end = bounds[0];
+    }
+  } else { // count number of affected lines
+    affectedLines = invert ? indentation.match(removeSpace).length : indentation.match(addSpace).length;
+    // add two spaces/backspaces before new lines
+    indentation = invert ? indentation.replace(removeSpace, "") : indentation.replace(addSpace, spaces);
+  }
+  // rebuild the textarea content
+  log("start length:", text.length);
+  newText = text.substring(0, start);
+  newText += indentation;
+  newText += text.substring(end);
+  log("end length:", newText.length);
+  ta.value = newText;
+  if(singleSelection) {
+    start = end = bounds[0] + (invert ? -numberOfIndents : numberOfIndents);
+  } else {
+    // add/substract end after indentation
+    invert ? (end -= affectedLines*numberOfIndents) : (end += affectedLines*numberOfIndents);
+  }
+  // reselect text
+  selectText(start, end);
+}
+
+function selectText(start, end) {
+  ta.setSelectionRange(start, end);
 }
 
 function run(code) {
@@ -187,5 +231,20 @@ function format(o) {
       return { name: value.toString(), category: value.category, filename: value.filename, line: value.lineNumber, stack: value.stack };
     }
     return value;
+  }
+}
+
+function polyfills() {
+  // from https://github.com/paulmillr/es6-shim/blob/master/es6-shim.js
+  if(!String.prototype.repeat) {
+    String.prototype.repeat = function(times) {
+      return repeat(this, times);
+    };
+    function repeat(s, times) {
+      if (times < 1) return '';
+      if (times % 2) return repeat(s, times - 1) + s;
+      var half = repeat(s, times / 2);
+      return half + half;
+    };
   }
 }
